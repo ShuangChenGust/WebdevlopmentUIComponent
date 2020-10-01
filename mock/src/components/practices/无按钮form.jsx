f255d7477a97826d897bb29125d1addebac2ad50
shuangchen <guschenwpi@gmail.com>
2020-09-30 20:34:38 -0400

    heap sort

---
+++ mock/src/components/practices/无按钮form.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 'Initial data...'
+      }
+      this.updateState = this.updateState.bind(this);
+   };
+   updateState(e) {
+      this.setState({data: e.target.value});
+   }
+   render() {
+      return (
+         <div>
+            <input type = "text" value = {this.state.data} 
+               onChange = {this.updateState} />
+            <h4>{this.state.data}</h4>
+         </div>
+      );
+   }
+}
+export default App;
+
+//main js
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App.jsx';
+
+// ReactDOM.render(<App/>, document.getElementById('app'));
